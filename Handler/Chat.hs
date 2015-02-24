86f86c46b9f8957305c63db4d3eed87281dc1d42
Rene Klacan <rene.klacan@reevoo.com>
2015-02-24 23:43:03 +0000

    Store messages in DB

--- Handler/Chat.hs
+++ Handler/Chat.hs
@@ -9,29 +9,34 @@ import Control.Concurrent.STM.Lifted
 
 chatApp :: TChan Text -> WebSocketsT Handler ()
 chatApp wChan = do
-    sendTextData ("Welcome to the chat server, please enter your name." :: Text)
-    name <- receiveData
-    sendTextData $ "Welcome, " <> name
-    rChan <- atomically $ do
-        writeTChan wChan $ name <> " has joined the chat"
-        dupTChan wChan
-    race_
-        (forever $ atomically (readTChan rChan) >>= sendTextData)
-        (sourceWS $$ mapM_C (\msg ->
-            atomically $ writeTChan wChan $ name <> ": " <> msg))
+  sendTextData ("Welcome to the chat server, please enter your name." :: Text)
+  name <- receiveData
+  sendTextData $ "Welcome, " <> name
+  rChan <- atomically $ do
+    writeTChan wChan $ name <> " has joined the chat"
+    dupTChan wChan
+  race_
+    (forever $ do
+      atomically (readTChan rChan) >>= sendTextData
+    )
+    (sourceWS $$ mapM_C (\msg -> do
+      _ <- lift . runDB $ insert $ Message msg 2 name
+      atomically $ do
+        writeTChan wChan $ name <> ": " <> msg
+    ))
 
 getChatR :: Int -> Handler Html
 getChatR roomId = do
-    App {..} <- getYesod
-    chan <- liftIO $ atomically $ do
-      m <- readTVar channels
-      case IntMap.lookup roomId m of
-        Nothing -> do
-          c <- newBroadcastTChan
-          writeTVar channels $ IntMap.insert roomId c m
-          return $ Just c
-        Just c -> fmap Just $ dupTChan c
-    case chan of
-      Nothing -> return ()
-      Just c -> webSockets $ chatApp c
-    defaultLayout $ $(widgetFile "chat")
+  App {..} <- getYesod
+  chan <- liftIO $ atomically $ do
+    m <- readTVar channels
+    case IntMap.lookup roomId m of
+      Nothing -> do
+        c <- newBroadcastTChan
+        writeTVar channels $ IntMap.insert roomId c m
+        return $ Just c
+      Just c -> fmap Just $ dupTChan c
+  case chan of
+    Nothing -> return ()
+    Just c -> webSockets $ chatApp c
+  defaultLayout $ $(widgetFile "chat")
