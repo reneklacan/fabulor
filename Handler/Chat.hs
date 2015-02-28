b6b1517e3274677ca3241a882ddf1211dedaf2cd
Rene Klacan <rene.klacan@reevoo.com>
2015-02-28 00:28:35 +0000

    Work only with json messages

--- Handler/Chat.hs
+++ Handler/Chat.hs
@@ -5,30 +5,8 @@ import Import hiding (atomically,(<>),ByteString)
 import Yesod.WebSockets
 import qualified Data.IntMap as IntMap
 import Control.Concurrent.STM.Lifted
-import Data.Aeson (encode)
+import Data.Aeson (encode,eitherDecode)
 import Data.ByteString.Lazy.Internal
-
-chatApp :: Int -> Text -> TChan ByteString -> WebSocketsT Handler ()
-chatApp roomId username wChan = do
-    jTime <- liftIO getCurrentTime
-    let joinMsg = Message roomId username "event" " has joined the room" jTime
-    let joinMsgDump = encode joinMsg
-    _ <- lift . runDB $ insert joinMsg
-    sendTextData joinMsgDump
-    rChan <- atomically $ do
-        writeTChan wChan joinMsgDump
-        dupTChan wChan
-    race_
-        (forever $ do
-            atomically (readTChan rChan) >>= sendTextData
-        )
-        (sourceWS $$ mapM_C (\text -> do
-            time <- liftIO getCurrentTime
-            let msg = Message roomId username "message" text time
-            _ <- lift . runDB $ insert msg
-            atomically $ do
-                writeTChan wChan $ encode msg
-        ))
 
 getChatR :: Int -> Handler Html
 getChatR roomId = do
@@ -47,3 +25,40 @@ getChatR roomId = do
         Just c -> webSockets $ chatApp roomId (userEmail user) c
     messages <- runDB $ selectList [MessageRoomId ==. roomId] [Asc MessageId]
     defaultLayout $ $(widgetFile "chat")
+
+chatApp :: Int -> Text -> TChan ByteString -> WebSocketsT Handler ()
+chatApp roomId username wChan = do
+    time <- liftIO getCurrentTime
+    let joinMsg = Message roomId username "event" " has joined the room" time
+    let joinMsgDump = encode joinMsg
+    _ <- lift . runDB $ insert joinMsg
+    sendTextData joinMsgDump
+    rChan <- atomically $ do
+        writeTChan wChan joinMsgDump
+        dupTChan wChan
+    race_
+        (forever $ atomically (readTChan rChan) >>= sendTextData)
+        (sourceWS $$ mapM_C (handleMsg roomId username wChan))
+
+handleMsg :: Int -> Text -> TChan ByteString -> ByteString -> WebSocketsT Handler ()
+handleMsg roomId username wChan text = do
+    case eitherDecode text of
+        Left e -> do
+            print e
+            return ()
+        Right baseMsg -> do
+            time <- liftIO getCurrentTime
+            let msg = baseMsg { messageRoomId = roomId
+                              , messageUsername = username
+                              , messageCreatedAt = time }
+            print msg
+            case messageType msg of
+                "message" -> broadcastMsg wChan msg
+                "event" -> broadcastMsg wChan msg
+                _ -> return ()
+
+broadcastMsg :: TChan ByteString -> Message -> WebSocketsT Handler ()
+broadcastMsg wChan msg = do
+    _ <- lift . runDB $ insert msg
+    atomically $ do
+        writeTChan wChan $ encode msg
--- Model.hs
+++ Model.hs
@@ -10,6 +10,15 @@ import Database.Persist.Quasi
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
 
+instance FromJSON Message where
+    parseJSON (Object v) = Message
+                          <$> v .: "roomId"
+                          <*> v .: "username"
+                          <*> v .: "type"
+                          <*> v .: "value"
+                          <*> v .: "createdAt"
+    parseJSON _ = mzero
+
 instance ToJSON Message where
     toJSON (Message roomId username mtype value createdAt)
         = object ["username" .= username
