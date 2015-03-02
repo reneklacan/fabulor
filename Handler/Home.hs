38cdf2d913baac054b29791599331c82f1379abf
Rene Klacan <rene.klacan@reevoo.com>
2015-03-02 00:17:23 +0000

    Move logic from Home handler into the RoomList

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,20 +1,8 @@
 module Handler.Home where
 
 import Import
---import Database.Persist.Sql (fromSqlKey)
 --import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3, withSmallInput)
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    _ <- requireAuthId
-    (Entity _ user) <- requireAuth
-    rooms <- runDB $ selectList [] [Desc RoomId]
-    defaultLayout $ do
-        $(widgetFile "homepage")
+    redirect RoomListR
---
+++ Handler/RoomList.hs
@@ -0,0 +1,12 @@
+module Handler.RoomList where
+
+import Import
+
+--import Database.Persist.Sql (fromSqlKey)
+
+getRoomListR :: Handler Html
+getRoomListR = do
+    (Entity _ user) <- requireAuth
+    rooms <- runDB $ selectList [] [Desc RoomId]
+    defaultLayout $ do
+        $(widgetFile "room-list")
