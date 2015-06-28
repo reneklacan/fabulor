8a94850c90b5ec052e6171810424c3843097c9aa
Rene Klacan <rene.klacan@gmail.com>
2015-06-28 22:32:54 +0200

    Display only available rooms

--- Handler/Rooms/RoomList.hs
+++ Handler/Rooms/RoomList.hs
@@ -2,11 +2,21 @@ module Handler.Rooms.RoomList where
 
 import Import
 
---import Database.Persist.Sql (fromSqlKey)
+import qualified Database.Esqueleto as E
+import Database.Esqueleto ((^.),(?.))
 
 getRoomListR :: Handler Html
 getRoomListR = do
-    (Entity _ user) <- requireAuth
-    rooms <- runDB $ selectList [] [Desc RoomId]
+    (Entity userId user) <- requireAuth
+    rooms <- runDB $ roomsQuery userId $ userIsAdmin user
     defaultLayout $ do
         $(widgetFile "room-list")
+  where
+    roomsQuery _ True = do
+        selectList [] [Desc RoomId]
+    roomsQuery userId False = do
+        E.select $
+            E.from $ \(room `E.LeftOuterJoin` room_access) -> do
+            E.on $ E.just (room ^. RoomId) E.==. room_access ?. RoomAccessRoomId
+            E.where_ $ room_access ?. RoomAccessUserId E.==. E.just (E.val userId)
+            return ( room )
